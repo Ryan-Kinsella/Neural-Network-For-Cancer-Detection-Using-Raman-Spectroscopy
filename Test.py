b0d48096a30a49409b424a358e720393c1f6eba3
Ryan-Kinsella <ryanklkinsella@gmail.com>
2019-10-29 16:37:29 -0400

    accuracy 0.420 in Test.py. All features being used to predict class labels

--- Test.py
+++ Test.py
@@ -13,7 +13,7 @@ Source 5: Tensorflow DNNClassifier, https://www.tensorflow.org/api_docs/python/t
 Source 6: https://towardsdatascience.com/getting-data-into-tensorflow-estimator-models-3432f404a8da
 Source 7: Someone can use sns.countplot and sns.heatmap as shown in this article to see correlations,
     https://medium.com/datadriveninvestor/tensorflow-dnnclassifier-4e68df3df00
-
+Source 8: Similar to Source 7, How to use a tensorflow pipeline for premade estimators, https://www.tensorflow.org/tutorials/estimator/premade
 
 
 Notes:
@@ -27,35 +27,31 @@ Ryan - The Github csv file does not contain an initial row creating the labels f
     in the top right corner saying "Cancer Type", instead they're currently labeled by "High-grade tumor-1",
     which is fine but something to refer to when referencing the last column.
      - I mainly used Source 7 as a template for the code in Test.py.
-
-
-
+    - 0.136482218, 0.14102496,... are the first two feature labels
+    - Using The Keras API is apparently easier to learn.
 
 
 """
-################## Source 7 Mainly used ##################
+################## Source 7 Mainly used, Source 8 helps explain as well ##################
 # Source 1 can be used to understand pre-processing the features and targets
 
-import math
+import math # unused
 from IPython import display # unused
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt # unused
+import numpy as np # unused
 import pandas as pd
 from sklearn import metrics # unused
 import tensorflow as tf
 import seaborn as sns # unused
-
-
 pd.options.display.max_rows = 8 # will use 8 by default for count, mean, std ... max
 pd.options.display.max_columns = 9
 pd.options.display.float_format = '{:.6f}'.format
-# display.display(validation_features.describe())
 
 dataset = pd.read_csv("Dataset_Github_csv.csv")
 # From DataFrame, split into features (x) and labels (y)
 x= dataset.drop(['High-grade tumor-1'], axis=1)
 y= dataset['High-grade tumor-1']
-# change y to be assigned to one of three classes: High-grade, Low-grade, Normal
+# change y in the csv file to be assigned to one of three classes: High-grade, Low-grade, Normal
 for i in range (0,323): # 0 - 322, same size as x
     #print(type(y[i]))
     if y[i].startswith('High-grade'):  # if the last column contains text "High-grade", etc below.
@@ -72,7 +68,7 @@ for i in range (0,323): # 0 - 322, same size as x
 #print(dataset.columns)
 ########### Find way to display like in source 7
 #dataset_seaborn = sns.load_dataset(name='https://github.com/Ryan-Kinsella/Neural-Network-For-Cancer-Detection-Using-Raman-Spectroscopy/blob/master/Dataset_Github_csv.csv')
-#sns.countplot(data=dataset_seaborn, x='0.136482218') #this only works in python running notebooks, Source 7
+#sns.countplot(data=dataset_seaborn, x='0.136482218') #this only works in python running notebooks like jupytor or whatever, Source 7
 #plt.show()
 
 
@@ -80,11 +76,11 @@ for i in range (0,323): # 0 - 322, same size as x
 from sklearn.preprocessing import LabelEncoder
 lbl_encoder = LabelEncoder()
 y= lbl_encoder.fit_transform(y)
-# print(y) # shows how the classes are numerically assigned through this change
+# print(y) # shows how the classes are numerically assigned through this change, by 0,1, or 2
 
 # Split data into train and test set
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test= train_test_split(x,y, test_size=0.25)
+x_train, x_test, y_train, y_test= train_test_split(x,y, test_size=0.25) # 75% training, 25% test
 
 # Convert numeric features into Dense Tensors, and construct the feature columns
 def construct_feature_columns(input_features_DataFrame):
@@ -95,51 +91,31 @@ def construct_feature_columns(input_features_DataFrame):
   Returns:
     A set of tf numeric feature columns
   """
-  return set([tf.feature_column.numeric_column(my_feature)
-              for my_feature in input_features_DataFrame])
-# def construct_feature_columns_2(x_labels):
-#   """Construct the TensorFlow Feature Columns.
-#
-#   Args:
-#     input_features: DataFrame with the names of the numerical input features to use.
-#   Returns:
-#     A set of tf numeric feature columns
-#   """
-#   aSet = set ([])
-#   for i in range(0,322):
-#       aSet.add(x_labels[0][i])
-#   return aSet
+  tensorSet = ([])
+  for elem in input_features_DataFrame:
+    tensorSet.append( tf.feature_column.numeric_column(str(elem)) ) # where elem is a str feature label
+  return tensorSet
+  # return set([tf.feature_column.numeric_column(my_feature)
+  #             for my_feature in input_features_DataFrame])
 
 x_labels = x.head(0) # gets the labels for x, with the dropped class column
-# x_labels_string = x_labels.to_string()
-# print(x_labels_string)
-# print("Type is:" , type(x_labels_string))
-# for col in x_labels_string:
-#     print (col)
-#x_labels_string.unique()
-#print (x_labels_string)
-# aSet = set (x_labels_string)
-# print("X labels: ", x_labels)
-# feature_col = construct_feature_columns_2(x_labels)
-# print("Feature Cols: ", feature_col)
-# print ("X_labels: ", x_labels_string)
-# print ("aSet", aSet)
+feature_columns=construct_feature_columns(x_labels)
+print(type(feature_columns))
 
 
 # Create the input function for training + evaluation
-def train_input_fn(x_training, y_training):
-    dataf = tf.data.Dataset.from_tensor_slices((dict(x_training), y_training))
-    dataf = dataf.shuffle(1000).repeat().batch(10)
-    return dataf
+def input_fn(features, labels, training=True, batch_size=100 ):
+    dataf = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    if training:
+        dataf = dataf.shuffle(1000).repeat()
+    return dataf.batch(batch_size=batch_size)
 def eval_input_fn(x_testing, y_testing):
     dataf = tf.data.Dataset.from_tensor_slices((dict(x_testing), y_testing))
     return dataf.shuffle(1000).repeat().batch(10)
 
 optimizer_adam= tf.compat.v1.train.AdamOptimizer(learning_rate=0.01)
-# NEED TO SET PROPER FEATURE_COLUMNS
-model=tf.estimator.DNNClassifier(hidden_units=[9], feature_columns=x_labels,  optimizer=optimizer_adam, n_classes=3)
-model.train(input_fn=lambda: train_input_fn(x_train, y_train), steps=100) # originally steps=1000 from template
-
-eval_results = model.evaluate(input_fn=eval_input_fn(x_test, y_test), steps=1)
-#print(eval_results)
+model=tf.estimator.DNNClassifier(hidden_units=[1,1], feature_columns=feature_columns,  optimizer=optimizer_adam, n_classes=3)
+model.train(input_fn=lambda: input_fn(features=x_train, labels=y_train, training=True), steps=1000) # originally steps=1000 from template
+eval_results = model.evaluate(input_fn=lambda: input_fn(features=x_test, labels=y_test, training=False), steps=1)
+print(eval_results)
 
