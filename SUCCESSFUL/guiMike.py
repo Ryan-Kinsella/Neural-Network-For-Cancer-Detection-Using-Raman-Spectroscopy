a8e167e39fdb90d10bf093474ed2ec07f2ffe8dc
Mike McColm <mike.mccolm28@gmail.com>
2020-02-12 21:37:43 -0500

    updates

--- SUCCESSFUL/guiMike.py
+++ SUCCESSFUL/guiMike.py
@@ -3,22 +3,69 @@ from tkinter import *
 from PIL import ImageTk, Image
 import os
 
-accuracyDNN, accuracyCNN, accuracySVM, accuracyTREE = ensemble.startup()
-print(accuracyDNN, accuracyCNN, accuracySVM, accuracyTREE)
+from tkinter import *
 
+accuracyDNN, accuracyCNN, accuracySVM, accuracyTREE, predictionsENS, predictionsDNN, predictionsCNN, predictionsSVM, predictionsTREE, predictionsEXP = ensemble.startup()
 
-DNNprediction = "Prediction: "
-CNNprediction = "Prediction: "
-TREEprediction = "Prediction: " 
-SVMprediction = "Prediction: "
-DNNacc = "Accuracy: " + ((str(accuracyDNN))[:5]) + "%"
-CNNacc = "Accuracy: " +((str(accuracyCNN))[:5]) + "%"
-TREEacc = "Accuracy: " +((str(accuracyTREE))[:5])+ "%"
-SVMacc = "Accuracy: " +((str(accuracySVM))[:5])+ "%"
-DNNconf = "Confidence: " + "%"
-CNNconf = "Confidence: " + "%"
-SVMconf = "Confidence: " + "%"
-TREEconf = "Confidence: " + "%"
+print(predictionsENS[0])
+count = 0
+
+def getStats(count,accuracyDNN, accuracyCNN, accuracySVM, accuracyTREE, predictionsENS, predictionsDNN, predictionsCNN, predictionsSVM, predictionsTREE, predictionsEXP):
+    DNNprediction = "Prediction: "
+    if predictionsDNN[count][0] >= predictionsDNN[count][1] and predictionsDNN[count][0]>=predictionsDNN[count][2]:
+        DNNprediction+= "Healthy"
+    elif predictionsDNN[count][1] >= predictionsDNN[count][0] and predictionsDNN[count][1]>=predictionsDNN[count][2]:
+        DNNprediction+= "Medium"
+    elif predictionsDNN[count][2] >= predictionsDNN[count][0] and predictionsDNN[count][2]>=predictionsDNN[count][1]:
+        DNNprediction+= "High"
+    CNNprediction = "Prediction: "
+    if predictionsCNN[count][0] >= predictionsCNN[count][1] and predictionsCNN[count][0]>=predictionsCNN[count][2]:
+        CNNprediction+= "Healthy"
+    elif predictionsCNN[count][1] >= predictionsCNN[count][0] and predictionsCNN[count][1]>=predictionsCNN[count][2]:
+        CNNprediction+= "Medium"
+    elif predictionsCNN[count][2] >= predictionsCNN[count][0] and predictionsCNN[count][2]>=predictionsCNN[count][1]:
+        CNNprediction+= "High"
+    TREEprediction = "Prediction: " 
+    if predictionsTREE[count][0] >= predictionsTREE[count][1] and predictionsTREE[count][0]>=predictionsTREE[count][2]:
+        TREEprediction+= "Healthy"
+    elif predictionsTREE[count][1] >= predictionsTREE[count][0] and predictionsTREE[count][1]>=predictionsTREE[count][2]:
+        TREEprediction+= "Medium"
+    elif predictionsTREE[count][2] >= predictionsTREE[count][0] and predictionsTREE[count][2]>=predictionsTREE[count][1]:
+        TREEprediction+= "High"
+    SVMprediction = "Prediction: "
+    if predictionsSVM[count][0] >= predictionsSVM[count][1] and predictionsSVM[count][0]>=predictionsSVM[count][2]:
+        SVMprediction+= "Healthy"
+    elif predictionsSVM[count][1] >= predictionsSVM[count][0] and predictionsSVM[count][1]>=predictionsSVM[count][2]:
+        SVMprediction+= "Medium"
+    elif predictionsSVM[count][2] >= predictionsSVM[count][0] and predictionsSVM[count][2]>=predictionsSVM[count][1]:
+        SVMprediction+= "High"
+    ENSprediction = "Ensemble Prediction: "
+    if predictionsENS[count] == 0:
+        ENSprediction+= "Healthy"
+    elif predictionsENS[count] == 1:
+        ENSprediction+= "Medium"
+    elif predictionsENS[count] == 2:
+        ENSprediction+= "High"
+    EXPprediction = "Actual: "
+    if predictionsEXP[count] == 0:
+        EXPprediction+= "Healthy"
+    elif predictionsEXP[count] == 1:
+        EXPprediction+= "Medium"
+    elif predictionsEXP[count] == 2:
+        EXPprediction+= "High"
+    DNNacc = "Accuracy: " + ((str(accuracyDNN))[:5]) + "%"
+    CNNacc = "Accuracy: " +((str(accuracyCNN))[:5]) + "%"
+    TREEacc = "Accuracy: " +((str(accuracyTREE))[:5])+ "%"
+    SVMacc = "Accuracy: " +((str(accuracySVM))[:5])+ "%"
+    DNNconf = "Confidence: " + (str(max(predictionsDNN[count]))[:5]) + "%"
+    CNNconf = "Confidence: " + (str(max(predictionsCNN[count]))[:5]) + "%"
+    SVMconf = "Confidence: " + (str(max(predictionsSVM[count]))[:5]) + "%"
+    TREEconf = "Confidence: " + (str(max(predictionsTREE[count]))[:5]) +"%"
+    sampleName = "Sample #" + str(count+1)
+    return ENSprediction, DNNprediction, CNNprediction, TREEprediction, SVMprediction, DNNacc, CNNacc, TREEacc, SVMacc, DNNconf, CNNconf,SVMconf, TREEconf, EXPprediction, sampleName
+
+ENSprediction, DNNprediction, CNNprediction, TREEprediction, SVMprediction, DNNacc, CNNacc, TREEacc, SVMacc, DNNconf, CNNconf,SVMconf, TREEconf, EXPprediction, sampleName = getStats(count, accuracyDNN, accuracyCNN, accuracySVM, accuracyTREE, predictionsENS, predictionsDNN, predictionsCNN, predictionsSVM, predictionsTREE, predictionsEXP)
+
 
 
 master = Tk()
@@ -27,12 +74,17 @@ master = Tk()
 w = Canvas(master, width=500, height=500)
 w.pack()
 w.create_rectangle(0,2, 500, 50, fill = "grey")  
-w.create_text(250,25, text="Sample #1")
+w.create_text(250,25, text=sampleName)
 w.create_line(0,275,500,275,width = 3)
 w.create_line(250,275,250,500, width = 3)
 w.create_text(125,290, text="Spectra:")
 w.create_text(375,290, text="Key Features:")
-w.create_text(250,250, text ="Ensemble Prediction: Non-cancerous",)
+if predictionsENS[count] == predictionsEXP[count]:
+    w.create_rectangle(0,225, 500, 275, fill = "#98FB98")  
+else:
+    w.create_rectangle(0,225, 500, 275, fill = "pink")  
+w.create_text(125,250, text = ENSprediction)
+w.create_text(375,250, text = EXPprediction)
 w.create_line(125,50,125,225, width = 3)
 w.create_line(250,50,250,225, width = 3)
 w.create_line(375,50,375,225, width = 3)
@@ -61,10 +113,11 @@ w.create_text(437,100, text = TREEprediction)
 w.create_text(437,135, text = TREEacc)
 w.create_text(437,170, text = TREEconf)
 
-img1 = ImageTk.PhotoImage(Image.open("doggie.png").resize((244, 195)))
-img2 = ImageTk.PhotoImage(Image.open("cathat.png").resize((247, 195)))
 
-w.create_image(0,305,anchor = NW, image = img1)
-w.create_image(256,305, anchor = NW, image = img2)
+# img1 = ImageTk.PhotoImage(Image.open("doggie.png").resize((244, 195)))
+# img2 = ImageTk.PhotoImage(Image.open("cathat.png").resize((247, 195)))
+
+# w.create_image(0,305,anchor = NW, image = img1)
+# w.create_image(256,305, anchor = NW, image = img2)
 
 mainloop()
